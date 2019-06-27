c61ab78ef5c8d8efbc60f8b7005c767127a8abbc
Carlos C Soto <eclipxe13@gmail.com>
2019-06-27 16:08:55 -0500

    Create test case with issues on IssuerName

--- tests/Unit/DOMSignerTest.php
+++ tests/Unit/DOMSignerTest.php
@@ -39,4 +39,33 @@ class DOMSignerTest extends TestCase
         $this->expectExceptionMessage('Document does not have a root element');
         $signer->sign($credentials);
     }
+
+    public function testCreateKeyInfoWithIssuerNameWithAmpersand(): void
+    {
+        $issuerName = 'John & Co';
+        $document = new DOMDocument();
+        $signer = new class($document) extends DOMSigner {
+            public function exposeCreateKeyInfoWithData(string $issuerName): DOMElement
+            {
+                return $this->createKeyInfoWithData($issuerName, '', '');
+            }
+
+            protected function obtainPublicKeyValues(string $publicKeyContents): array
+            {
+                return [
+                    'type' => OPENSSL_KEYTYPE_RSA,
+                    'rsa' => ['n' => '1', 'e' => '2'],
+                ];
+            }
+        };
+
+        /** @var DOMElement $keyInfo */
+        $keyInfo = $signer->exposeCreateKeyInfoWithData($issuerName);
+
+        $this->assertXmlStringEqualsXmlString(
+            '<X509IssuerName>John &amp; Co</X509IssuerName>',
+            $document->saveXML($keyInfo->getElementsByTagName('X509IssuerName')[0]),
+            'Ampersand was not correctly parsed'
+        );
+    }
 }
